cda8e92f6e44922e38fd72f7b055dc920fabc344
sivananda <cvasia549@gmail.com>
2024-11-26 16:41:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+// import Logo from "./assets/logo.png";
+import { fetch } from './axios';
+
+function App() {
+  const [link, setLink] = useState('');
+  const [id, setId] = useState(null);
+  const [response, setResponse] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  
+  useEffect(() => {
+    if (id) {
+      const fetchData = () => {
+        let interval = setInterval(async function() {
+          setDisabled(true);
+          const res = await fetch(id);
+          
+          if (res.status === 200 && res.data.status === "ok") {
+            setDisabled(false);
+            setResponse(res.data);
+            clearInterval(interval);
+          } else if (res.status === 200 && res.data.status === "fail") {
+            alert("Invalid video ID");
+            setDisabled(false);
+            clearInterval(interval);
+          }
+
+        }, 1000);
+      }
+
+      fetchData();
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (response) {
+      window.location.href = response.link;
+    }
+  }, [response]);
+
+  return (
+    <div className="App shadow">
+      <div id="logo">
+        {/* <img src={Logo} /> */}
+        <h2>MEGA CONVERTER MP3 DOWNLOADER</h2>
+      </div>
+
+      <div id="body">
+        <input
+          type="text"
+          placeholder="YouTube link here"
+          value={link}
+          onChange={(e) => {
+            setLink(e.target.value);
+          }}
+        />
+        <span>It might take a moment to convert your video</span>
+      </div>
+
+      <button
+        onClick={() => {
+          const text = link.split("=")[1];
+          if (text) {
+            setId(text);
+          }
+        }}
+        disabled={disabled}
+        className={disabled ? "btn-disabled" : ""}
+      >Download</button>
+    </div>
+  )
+}
+
+export default App
