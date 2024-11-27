550e1c5ade0232cffc0b4556b7b0568032ef4d96
sivananda <cvasia549@gmail.com>
2024-11-27 15:25:48 +0530

    first commit12

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { fetch } from "./axios";
-
+import { HiOutlineDotsVertical } from "react-icons/hi";
 function App() {
   const [link, setLink] = useState("");
   const [id, setId] = useState(null);
   const [response, setResponse] = useState(null);
   const [disabled, setDisabled] = useState(false);
-
+  const [linkmp4, setLinkmp4] = useState("");
   useEffect(() => {
     if (id) {
       const fetchData = () => {
@@ -51,9 +51,30 @@ function App() {
     return match ? match[1] : null;
   };
 
+  const handleInputChange = (e) => {
+    setLinkmp4(e.target.value);
+  };
+
+  const handleDownload = () => {
+    if (linkmp4 !== "") {
+      let YTurl;
+      if (linkmp4.includes("https://youtu.be/")) {
+        YTurl = linkmp4.replace(
+          "https://youtu.be/",
+          "https://www.000tube.com/watch?v="
+        );
+      } else {
+        YTurl = linkmp4.replace("you", "000");
+      }
+      window.open(YTurl, "_blank");
+    }
+  };
+
   return (
     <div className="App shadow">
-      <div id="logo">{/* <img src={Logo} /> */}<h2>MEGA CONVERTER MP3 DOWNLOADER</h2></div>
+      <div id="logo">
+        <h2>MEGA CONVERTER MP3 DOWNLOADER</h2>
+      </div>
 
       <div id="body">
         <input
@@ -81,6 +102,28 @@ function App() {
       >
         Download
       </button>
+      <div className="py-3"></div>
+      <div id="logo" className="fw-bold">
+        <h2 className="fw-bold">MEGA CONVERTER MP4 VEDIO DOWNLOADER</h2>
+      </div>
+      
+      <div id="body">
+        <input
+          type="text"
+          className="link"
+          value={linkmp4}
+          onChange={handleInputChange}
+          placeholder="Enter YouTube link"
+        />
+        <span>
+          {" "}
+          click the Download button to open a new tab, choose an option, cancel
+          the next tab, return to the options tab, and click the{" "}
+          <HiOutlineDotsVertical /> icon to download the MP4.
+        </span>
+      </div>
+      <br />
+      <button onClick={handleDownload}>Download Video</button>
     </div>
   );
 }
